2119f237e3aaaf8b85fd1f402540dba0da5d92c7
Yordy-code81 <U201923959@upc.edu.pe>
2022-06-07 16:42:21 -0500

    feat: base project

--- src/main/java/com/cybersoft/asimovapi/AsimovApiApplication.java
+++ src/main/java/com/cybersoft/asimovapi/AsimovApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AsimovApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AsimovApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AsimovApiApplication.class, args);
+    }
 
 }
